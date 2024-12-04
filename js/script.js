c42822ec226c7f831779af5bea484f88a3da39b2
Artem Abramov <artemabramov230999@gmail.com>
2024-12-04 15:20:33 +0100

    add section banner and features

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-	
-	
+  
+  // Banner Swiper
+	const swiperBanner = new Swiper('.banner-swiper', {
+    speed: 700,
+    spaceBetween: 50, 
+    slidesPerView: 1,
+    pagination: {
+      enabled: true,
+      el: '.banner-swiper .swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      enabled: true,
+      prevEl: '.banner-swiper .swiper-button-prev',
+      nextEl: '.banner-swiper .swiper-button-next',
+      clickable: true
+    }
+  });
 
 }, false);
